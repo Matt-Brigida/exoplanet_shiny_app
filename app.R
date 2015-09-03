b8ee6393a54c4afedefd76e4ebaa66ed17c24fd9
Matt Brigida <matt@complete-markets.com>
2015-09-03 18:46:10 -0400

    Added tabs and 3D scatterplot on other tab.

--- app.R
+++ app.R
@@ -1,10 +1,16 @@
 ## Get and display exoplanet data ----
+## app pulls data twice, fix to only require one ----
 
 library(DT)
+library(threejs)
 
 ui <- bootstrapPage(
-  DT::dataTableOutput("datatable"))
-
+        tabsetPanel(type = "tabs",
+                    tabPanel("Data", DT::dataTableOutput("datatable")),
+                    tabPanel("Plot", threejs::scatterplotThreeOutput("plot"))
+                    )
+    )
+    
 server <- function(input, output){
 
 output$datatable <- DT::renderDataTable({ 
@@ -23,6 +29,31 @@ names(data) <- c("Primary ID", "Binary Flag", "Planetary Mass", "Radius", "Perio
 DT::datatable(data[, c("Primary ID", "Binary Flag", "Planetary Mass", "Discovery Year", "Distance from Sun (parsec)")], options = list(pageLength = 15))
 
 })
+
+output$plot <- threejs::renderScatterplotThree({
+
+url <- "https://raw.githubusercontent.com/OpenExoplanetCatalogue/oec_tables/master/comma_separated/open_exoplanet_catalogue.txt"
+library(RCurl)
+## x <- getURL(url)
+## get data
+data <- read.csv(url, skip = 30, stringsAsFactors = F)
+## column names
+names(data) <- c("Primary ID", "Binary Flag", "Planetary Mass", "Radius", "Period", "Axis", "Eccentricity", "Periastron", "Longitude" ,"Ascending Node", "Inclination", "Temp", "Age", "Discovery Method" ,"Discovery Year", "Last Updated", "Right Ascension", "Declination", "Distance from Sun (parsec)", "Host Start Mass", "Host Star Radius", "HS Metallicity", "HS temp" ,"HS age")
+
+
+data.three <- data[,c(3,15,19)]
+data.three[,1] <- log(data.three[,1])
+data.three[,3] <- log(data.three[,3])
+data.three2 <- data.three[complete.cases(data.three),]
+
+## order by year
+data.three2 <- data.three2[ order(-data.three2[,2]),]
+
+    scatterplot3js(x = data.three2, labels = data$'Primary ID', color = rainbow(length(data.three2[,2])), label.margin = TRUE, flip.y = TRUE)
+
+})
+
+
 }
 
 shinyApp(ui = ui, server = server)
